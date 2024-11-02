0079eddeafe936011311ec9df1efe3a5bc0919b2
Ирина Черняева <iridache@Irinas-MacBook-Air.local>
2024-11-02 10:35:59 +0300

    add router basename

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
-    path: "/meals-search-vue/",
+    path: "/",
     name: "home",
     component: Home,
   },
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/meals-search-vue/"),
   routes,
 });
 
